2a0f07fffbc42e44f9ecdaec46e67494de135c6e
matthewvolny <matthew.volny@gmail.com>
2021-12-21 19:32:16 -0500

    first commit

---
+++ scripts/scripts.js
@@ -0,0 +1,21 @@
+//adds "in-viewport" class to timeline elements upon entering the viewport
+const timelineElements = document.querySelectorAll(
+  ".timeline-container ul li > div"
+);
+function addInViewToElements() {
+  for (i = 0; i < timelineElements.length; i++) {
+    //topSide = timelineElements[i].getBoundingClientRect().top;
+    //rightSide = timelineElements[i].getBoundingClientRect().right;
+    bottomSide = timelineElements[i].getBoundingClientRect().bottom;
+    //leftSide = timelineElements[i].getBoundingClientRect().left;
+    //height = timelineElements[i].getBoundingClientRect().height;
+    //width = timelineElements[i].getBoundingClientRect().width;
+    let viewportHeight = document.documentElement.clientHeight;
+    if (bottomSide <= viewportHeight) {
+      timelineElements[i].setAttribute("id", "in-viewport");
+      //timelineElements[i].classList.remove("in-viewport");
+    }
+  }
+}
+
+window.addEventListener("scroll", addInViewToElements);
