3915978508318d2149cdd36778455f21a9653d68
matthewvolny <matthew.volny@gmail.com>
2021-12-22 21:55:54 -0500

    rover location data plotted on map, resizing and data transformation needed

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,70 @@
+let curiosityWaypointLatitudes = [];
+let curiosityWaypointLongitudes = [];
+let curiosityWaypointElevations = [];
+let curiostyWaypointMilesTraveled = [];
+let curiosityWaypointSol = [];
+
+//draw select nodes on curiosity map (look into which should be included)
+const drawWaypointDomElements = (waypoints) => {
+  const roverRouteMap = document.querySelector(".rover-route");
+  waypoints.ForEach((waypoint) => {
+    if (
+      waypoint.properties.sol === 20 ||
+      waypoint.properties.sol === 129 ||
+      waypoint.properties.sol === 180 ||
+      waypoint.properties.sol === 283
+    ) {
+      let waypointDomElement = document.createElement("div");
+      waypointDomElement.classList("waypoint-dom-element");
+      waypointDomElement.style.left = waypoint.properties.lat;
+      waypointDomElement.style.top = waypoint.properties.long;
+      roverRouteMap.appendChild(waypointDomElement);
+    }
+  });
+};
+
+//put interactive nodes on the the map
+const body = document.querySelector("body");
+body.addEventListener("click", (event) => {
+  if (event.target.classList.contains("RoverDot")) {
+    //pop up info for the day and info about the mars geography
+  }
+});
+
+async function getCuriosityLocationData() {
+  const response = await fetch("./assets/Waypoints-Perseverance.geojson");
+  const locationData = await response.json();
+  const waypoints = locationData.features;
+  console.log(locationData.features);
+  // waypoints.forEach((waypoint) => {
+  //   curiosityWaypointLatitudes.push(waypoint.geometry.coordinates);
+  // });
+
+  const drawCuriosityRoute = (waypoints) => {
+    const canvas = document.querySelector(".rover-route");
+    const ctx = canvas.getContext("2d");
+    ctx.beginPath();
+
+    ctx.moveTo(400, 400); // moves the pen to the
+    waypoints.forEach((waypoint) => {
+      //calculation to change waypoint.properties.lon and lat to x and y under 500px
+      console.log("hello");
+      let longitude = (waypoint.properties.lon - 77.4) * 15000;
+      let latitude = (waypoint.properties.lat - 18.4) * 15000;
+      ctx.lineTo(longitude, latitude);
+      console.log(`${longitude}, ${latitude}, ${waypoint.properties.sol}`);
+    });
+    ctx.stroke();
+    ///////
+    ctx.beginPath();
+    ctx.fillStyle = "green";
+    ctx.fillRect(763, 669, 5, 5);
+  };
+  drawCuriosityRoute(waypoints);
+  drawWaypointDomElements(waypoints);
+}
+getCuriosityLocationData();
+
 //adds "in-viewport" class to timeline elements upon entering the viewport
 const timelineElements = document.querySelectorAll(
   ".timeline-container ul li > div"
