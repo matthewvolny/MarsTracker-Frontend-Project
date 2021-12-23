e0550f9379904eb6e02b1c7f6cb2ba8d5e8bb4b2
matthewvolny <matthew.volny@gmail.com>
2021-12-23 14:02:03 -0500

    rover position data mapped, not yet centered in canvas

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,7 +7,7 @@ let curiosityWaypointSol = [];
 //draw select nodes on curiosity map (look into which should be included)
 const drawWaypointDomElements = (waypoints) => {
   const roverRouteMap = document.querySelector(".rover-route");
-  waypoints.ForEach((waypoint) => {
+  waypoints.forEach((waypoint) => {
     if (
       waypoint.properties.sol === 20 ||
       waypoint.properties.sol === 129 ||
@@ -15,9 +15,9 @@ const drawWaypointDomElements = (waypoints) => {
       waypoint.properties.sol === 283
     ) {
       let waypointDomElement = document.createElement("div");
-      waypointDomElement.classList("waypoint-dom-element");
-      waypointDomElement.style.left = waypoint.properties.lat;
-      waypointDomElement.style.top = waypoint.properties.long;
+      waypointDomElement.classList.add("waypoint-dom-element");
+      // waypointDomElement.style.left = waypoint.properties.lat;
+      // waypointDomElement.style.top = waypoint.properties.long;
       roverRouteMap.appendChild(waypointDomElement);
     }
   });
@@ -44,21 +44,82 @@ async function getCuriosityLocationData() {
     const canvas = document.querySelector(".rover-route");
     const ctx = canvas.getContext("2d");
     ctx.beginPath();
-
-    ctx.moveTo(400, 400); // moves the pen to the
+    ctx.moveTo(175, 175); // moves the pen to the
+    //////   //////
+    let longitudeArray = [];
+    let latitudeArray = [];
+    ///////    ///////
     waypoints.forEach((waypoint) => {
       //calculation to change waypoint.properties.lon and lat to x and y under 500px
       console.log("hello");
-      let longitude = (waypoint.properties.lon - 77.4) * 15000;
-      let latitude = (waypoint.properties.lat - 18.4) * 15000;
-      ctx.lineTo(longitude, latitude);
-      console.log(`${longitude}, ${latitude}, ${waypoint.properties.sol}`);
+      ///////////  //////
+      longitudeArray.push(waypoint.properties.lon);
+      latitudeArray.push(waypoint.properties.lat);
+      /////////    //////
+      // console.log(longitudeArray);
+      // console.log(latitudeArray);
+      // let longitude = (waypoint.properties.lon - 77.4) * 5000 - 180;
+      // let latitude = -((waypoint.properties.lat - 18.4) * 5000 - 180);
+      //let longitude = (waypoint.properties.lon - 77.4) * 5000;
+      //let latitude = (waypoint.properties.lat - 18.4) * 5000;
+
+      // let x = (325 * (180 + latitude)) / 360;
+      // let y = (325 * (90 - longitude)) / 180;
+
+      // let newX = (x - 179) * 1000;
+      // let newY = (y - 22.6) * 1000;
+      // ctx.lineTo(longitude, latitude);
+      // ctx.lineTo(x, y);
+      //ctx.lineTo(longitude, latitude);
+      //console.log(`${longitude}, ${latitude}, ${waypoint.properties.sol}`);
+      // console.log(`${x}, ${y}`);
+      // console.log(`${newX}, ${newY}`);
+      // ctx.lineTo(longitude, latitude);
+      // console.log(`${longitude}, ${latitude}, ${waypoint.properties.sol}`);
     });
+
+    ///////   ////////
+    let lowestLatitudeValue = Math.min.apply(null, latitudeArray);
+    console.log(lowestLatitudeValue);
+    //let longitudeIndex = longitudeArray.indexOf(lowestLongitudeValue);
+    // console.log(longitudeIndex); //index of lowest value in array
+    let newLatitudeArray = latitudeArray.map((latitudeValue) => {
+      let latitudeDifference = latitudeValue - lowestLatitudeValue;
+      console.log(latitudeDifference);
+      return lowestLatitudeValue - latitudeDifference; //i believe this is "minus"
+    });
+
+    // let newLongitudeArray = longitudeArray.map((longitudeValue) => {
+    // return (longitudeValue - lowestLongitudeValue) * 15000 + 150;
+    // // });
+    console.log(newLatitudeArray); //each value in the longitude array minus the lowest value
+
+    ////////   ///////
+    // console.log(latitudeArray[longitudeIndex]); // lowest value in latitude array
+    // let newLatitudeArray = latitudeArray.map((latitudeValue) => {
+    //   return -(latitudeValue - latitudeArray[longitudeIndex]) * 15000 + 150;
+    // });
+    //console.log(newLatitudeArray);
+    console.log(longitudeArray);
+    ////  ////
+    ctx.beginPath();
+    ctx.moveTo(175, 175);
+    for (i = 0; i < newLatitudeArray.length; i++) {
+      longitudeArray[i] = (longitudeArray[i] - 77.4) * 5000;
+      newLatitudeArray[i] = (newLatitudeArray[i] - 18.4) * 5000;
+      // console.log(newLongitudeArray);
+      // console.log(latitudeArray);
+      ctx.lineTo(longitudeArray[i], newLatitudeArray[i]);
+    }
+    ////    /////
+
     ctx.stroke();
-    ///////
+    ///////  ///
     ctx.beginPath();
     ctx.fillStyle = "green";
-    ctx.fillRect(763, 669, 5, 5);
+    ctx.fillRect(100, 300, 10, 10);
+
+    /////   ////
   };
   drawCuriosityRoute(waypoints);
   drawWaypointDomElements(waypoints);
