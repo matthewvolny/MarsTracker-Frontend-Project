d07594eee9acc9a87c69049668eb230abf597fa8
Christy Crook <ccrook3@gmail.com>
2022-01-02 15:22:19 -0600

    got initial api call for curiosity photos

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,34 +1,9 @@
-//adds "in-viewport" class to timeline elements upon entering the viewport
-const timelineElements = document.querySelectorAll(
-    ".timeline-container ul li > div"
-  );
-  function addInViewToElements() {
-    for (i = 0; i < timelineElements.length; i++) {
-      //topSide = timelineElements[i].getBoundingClientRect().top;
-      //rightSide = timelineElements[i].getBoundingClientRect().right;
-      bottomSide = timelineElements[i].getBoundingClientRect().bottom;
-      //leftSide = timelineElements[i].getBoundingClientRect().left;
-      //height = timelineElements[i].getBoundingClientRect().height;
-      //width = timelineElements[i].getBoundingClientRect().width;
-      let viewportHeight = document.documentElement.clientHeight;
-      if (bottomSide <= viewportHeight) {
-        timelineElements[i].setAttribute("id", "in-viewport");
-        //timelineElements[i].classList.remove("in-viewport");
-      }
-    }
-  }
-  
-  window.addEventListener("scroll", addInViewToElements);
-
-  async function getRoverInfo() {
-    const characterResponse = await fetch(
-        `https://www.swapi.tech/api/people/?name=${urlEncodedSearchString}`
+// fetches curiosity photo 
+async function getCuriosityPhoto() {
+    const response = await fetch(
+        `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY`
       );
-      const rawCharacterData = await characterResponse.json();
-      const characterData = rawCharacterData.result;
-      charactersArray = [];
-      characterData.forEach((character) => {
-        charactersArray.push(character.properties.name);
-        getCharacterHomeWorld(character.properties.homeworld);
-      });
-  }
+      const photoData = await response.json();
+      return photoData;
+}
+getCuriosityPhoto().then(photoData => console.log(photoData));
